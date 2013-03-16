ab68b124760548c8e40753f99c15bd4a79df028c
Séverin Lemaignan <severin@guakamole.org>
2013-03-16 11:06:50 +0100

    Updated configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = u'S\xe9verin Lemaignan'
-SITENAME = u'skadge log'
-SITEURL = ''
+AUTHOR = u'Séverin Lemaignan'
+SITENAME = u'skadge\'s log'
+SITEURL = 'http://skadge.org'
+
+STATIC_PATHS = ["images", "videos"]
 
 TIMEZONE = 'Europe/Paris'
 
+DEFAULT_DATE = 'fs'
 DEFAULT_LANG = u'en'
 
 # Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+#LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+#          ('Python.org', 'http://python.org'),
+#          ('Jinja2', 'http://jinja.pocoo.org'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 10
+#DEFAULT_PAGINATION = 10
+
+#TYPOGRIFY = True
+
+GITHUB_URL = 'http://github.com/skadge'
